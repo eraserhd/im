eadaa2b58e768556d06b0ba48b78c1dbab388ad8
Jason Felice <jason.m.felice@gmail.com>
2010-09-01 10:18:07 -0400

    Make Field::Box more general

--- lib/field.rb
+++ lib/field.rb
@@ -1,28 +1,54 @@
 
-require 'guy'
-
 class Field
 
   class Box
-    def initialize(layer, x1, y1, x2, y2, solid)
-      @layer = layer
-      @x1, @y1 = x1, y1
-      @x2, @y2 = x2, y2
-      @solid = solid
+    attr_reader :x1, :y1, :x2, :y2
 
-      @x1, @x2 = @x2, @x1 if @x2 < @x1
-      @y1, @y2 = @y2, @y1 if @y2 < @y1
+    def solid?
+      true
     end
-
-    attr_reader :layer, :x1, :y1, :x2, :y2
-
-    def solid?; @solid; end
-    def hollow?; !@solid; end
 
     def intersects?(b)
       return false if x2 < b.x1 || b.x2 < x1
       return false if y2 < b.y1 || b.y2 < y1
       true
+    end
+
+    def hollow?
+      !solid?
+    end
+
+  protected
+    def self.layer(n)
+      (class << self; self; end).instance_eval do
+        define_method("layer") do
+          n
+        end
+      end
+    end
+
+    def self.solid
+      (class << self; self; end).instance_eval do
+        define_method("solid?") do
+          true
+        end
+      end
+    end
+
+    def self.hollow
+      (class << self; self; end).instance_eval do
+        define_method("solid?") do
+          false
+        end
+      end
+    end
+
+    def set_position(x1, y1, x2, y2)
+      @x1, @y1 = x1, y1
+      @x2, @y2 = x2, y2
+
+      @x1, @x2 = @x2, @x1 if @x2 < @x1
+      @y1, @y2 = @y2, @y1 if @y2 < @y1
     end
   end
 
@@ -35,8 +61,15 @@ class Field
   end
 
   def boxes_intersecting(x1, y1, x2, y2)
-    test_box = Field::Box.new(-1, x1, y1, x2, y2, false)
+    test_box = BBox.new(x1, y1, x2, y2)
     @boxes.select{|b| b.intersects?(test_box)}.sort{|a,b| a.layer <=> b.layer}
+  end
+
+private
+  class BBox < Box
+    def initialize(x1,y1,x2,y2)
+      set_position(x1,y1,x2,y2)
+    end
   end
 
 
--- spec/field_spec.rb
+++ spec/field_spec.rb
@@ -3,10 +3,21 @@ require 'rubygems'
 require 'gosu'
 require 'field'
 
+class TestBox < Field::Box
+
+  layer 1
+  hollow
+
+  def initialize(x1, y1, x2, y2)
+    set_position(x1,y1,x2,y2)
+  end
+
+end
+
 describe Field::Box do
 
   it "should preserve coordinates" do
-    @b = Field::Box.new(1, 30, 0, 40, 10, true)
+    @b = TestBox.new(30, 0, 40, 10)
     @b.x1.should == 30
     @b.x2.should == 40
     @b.y1.should == 0
@@ -14,28 +25,13 @@ describe Field::Box do
   end
 
   it "should correct ordering of x coordinates if necessary" do
-    @b = Field::Box.new(1, 40, 10, 30, 0, true)
+    @b = TestBox.new(40, 10, 30, 0)
     @b.x1.should == 30
     @b.x2.should == 40
     @b.y1.should == 0
     @b.y2.should == 10
   end
 
-  it "should preserve layer" do
-    @b = Field::Box.new(42, 40, 10, 30, 0, true)
-    @b.layer.should == 42
-  end
-
-  it "should preserve solidity" do
-    @b = Field::Box.new(42, 40, 10, 30, 0, true)
-    @b.solid?.should == true
-    @b.hollow?.should == false
-
-    @b = Field::Box.new(42, 40, 10, 30, 0, false)
-    @b.solid?.should == false
-    @b.hollow?.should == true
-  end
-  
 end
 
 describe Field, "hit testing" do
@@ -46,7 +42,7 @@ describe Field, "hit testing" do
 
   def query
     result = @f.boxes_intersecting(10,10,20,20)
-    result.each { |b| b.class.should == Field::Box }
+    result.each { |b| b.class.should == TestBox }
     result
   end
 
@@ -57,19 +53,19 @@ describe Field, "hit testing" do
   end
 
   it "should find a box which contains query" do
-    b = Field::Box.new(10, 0, 0, 40, 40, true)
+    b = TestBox.new(0, 0, 40, 40)
     @f.add(b)
     query.should == [b]
   end
 
   it "should find a box which partially intersects" do
-    b = Field::Box.new(10, 5, 5, 15, 15, true)
+    b = TestBox.new(5, 5, 15, 15)
     @f.add(b)
     query.should == [b]
   end
 
   it "should not find a box which does not intersect" do
-    b = Field::Box.new(10, 5, 5, 9, 15, true)
+    b = TestBox.new(5, 5, 9, 15)
     @f.add(b)
     query.should == []
   end
@@ -83,7 +79,7 @@ describe Field, "hit testing" do
       x2 = rand(20)
       y1 = rand(20)
       y2 = rand(20)
-      @b = Field::Box.new(42, x1, y1, x2, y2, true)
+      @b = TestBox.new(x1, y1, x2, y2)
       @f.add(@b)
 
       found = @f.boxes_intersecting(5,7,10,12).size > 0
@@ -112,7 +108,14 @@ describe Field, "hit testing" do
     10.times do
       @f = Field.new
       10.times do 
-        @f.add(Field::Box.new(rand(10), 0, 0, 40, 40, true))
+        tb = TestBox.new(0,0,40,40)
+        l = rand(10)
+        tb.instance_eval do
+          (class << self; self; end).instance_eval do
+            define_method("layer") { l }
+          end
+        end
+        @f.add(tb)
       end
       l = -1
       @f.boxes_intersecting(10,10,20,20).each do |b|
